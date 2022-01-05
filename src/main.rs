880b2084c5326db3374d02ed0e787402a1a76272
kelvinkirima014 <kelvinkirima2@gmail.com>
2022-01-05 20:46:30 +0300

    greet func

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use actix_web::{web, HttpRequest, Responder, App, HttpServer};
-
+use std::io;
 async fn greet(req: HttpRequest) -> impl Responder{
 	let name = req.match_info().get("name").unwrap_or("World");
 	format!("Hello {}!", name)
 }
 #[actix_web::main]
-async fn main() -> std::io::Result<()> {
+async fn main() -> io::Result<()> {
 	HttpServer::new(|| {
 		App::new()
 		.route("/", web::get().to(greet))
