ab4c28596bd2ee65488efd99571b439dd601d0a9
kelvinkirima014 <kelvinkirima2@gmail.com>
2022-01-06 13:28:24 +0300

    test get_response

--- tests/get_response.rs
+++ tests/get_response.rs
@@ -1,6 +1,19 @@
-use troperust::main;
+#[tokio::test]
+async fn get_response_works(){
+	spawn_app().await.expect("Failed to spawn our app");
 
-#[test]
-fn dummy_test() {
-	main()
+	let client = reqwest::Client::new();
+
+	let response = client
+		.get("http://127.0.0.1:8080/get_response")
+		.send()
+		.await
+		.expect("Failed to execute our request");
+
+		assert!(response.status().is_success());
+		assert_eq!(Some(0), response.content_length());
+}
+//launch app in background
+async fn spawn_app() -> std::io::Result<()> {
+	troperust::run().await
 }
