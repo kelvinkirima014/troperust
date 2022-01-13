bcd4dd0621cf73e0b520db3151964fb4f8f04e2b
kelvinkirima014 <kelvinkirima2@gmail.com>
2022-01-13 10:47:53 +0300

    spawn app, test for response

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use troperust::run;
 use std::io;
+use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
-	run()?
+	let address = TcpListener::bind("127.0.0.1:8000")?;
+	run(address)?.await
 }
--- tests/get_response.rs
+++ tests/get_response.rs
@@ -1,10 +1,10 @@
 #[tokio::test]
 async fn get_response_works(){
-	spawn_app();
+	let address = spawn_app();
 	let client = reqwest::Client::new();
 
 	let response = client
-		.get("http://127.0.0.1:8080/get_response")
+		.get(&format!("{}/get_response", &address))
 		.send()
 		.await
 		.expect("Failed to execute our request");
@@ -12,8 +12,16 @@ async fn get_response_works(){
 		assert!(response.status().is_success());
 		assert_eq!(Some(0), response.content_length());
 }
-//launch app in background
-fn spawn_app(){
-	let server = troperust::run("127.0.0.1:0").expect("Failed to bind address");
-	let _ = tokio::spawn(server);
+use std::net::TcpListener;
+fn spawn_app() -> String {
+	let listener = TcpListener::bind("127.0.0.1:0")
+	.expect("Failed to bind random port");
+	//retrieve port from OS
+	let port = listener.local_addr().unwrap()
+	.port();
+	let server = troperust::run(listener)
+	.expect("Failed to bind address");
+	let _ =  tokio::spawn(server);
+	//return the application address to caller
+	format!("http://127.0.0.1:{}", port)
 }
