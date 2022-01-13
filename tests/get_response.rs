8f4cc5b15c1a34adb0dd35822fbae51dbc55cd2b
kelvinkirima014 <kelvinkirima2@gmail.com>
2022-01-13 13:10:51 +0300

    test for missing data

--- tests/get_response.rs
+++ tests/get_response.rs
@@ -25,3 +25,59 @@ fn spawn_app() -> String {
 	//return the application address to caller
 	format!("http://127.0.0.1:{}", port)
 }
+
+#[tokio::test]
+async fn suscribe_returns_a_200_for_valid_form_data(){
+	let app_address = spawn_app();
+	let client = reqwest::Client::new();
+
+	Mock::given(path("/email"))
+	     .and(method("POST"))
+	     .respond_with(ResponseTemplate::new(200))
+	     .mount(&app.email_server)
+	     .await;
+
+	let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+	let response = client
+		.post(&format!("{}/subscriptions", &app_address))
+		.body(body) 
+		.send()
+		.await
+		.expect("Failed to load request");
+	
+		assert_eq!(200, response.status().as_u16());
+}
+#[tokio::test]
+async fn subscribe_returns_a_400_when_data_is_missing() {
+	let app_address = spawn_app();
+	let client = reqwest::Client::new();
+	let test_cases = vec![
+		("name=le%20guin", "missing the email"),
+		("email=ursula_le_guin%40gmail.com", "missing the name"),
+		("", "missing both name and email")
+	];
+	for (invalid_body, error_message) in test_cases {
+		let response = client
+			.post(&format!("{}/subscriptions", &app_address))
+			.header("Content-Type", "application/x-www-form-urlencoded")
+			.body(invalid_body)
+			.send()
+			.await
+			.expect("failed to execute request");
+
+		assert_eq!(
+			400,
+			response.status().as_u16(),
+			//custom error msg on test failure
+			"The API did not failed with 400 Bad Request when the payload was {}.",
+			error_message
+
+		);
+	}
+}
+
+
+
+
+
+
