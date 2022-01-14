8d05f09497f68b958601742411004275410fd775
kelvinkirima014 <kelvinkirima2@gmail.com>
2022-01-14 22:00:50 +0300

    add serde as dev dep

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,12 @@ async fn get_response() -> HttpResponse {
 	HttpResponse::Ok().finish()
 }
 //return a 200 OK response
-async fn subscribe() -> HttpResponse {
+#[derive(serde::Deserialize)]
+struct FormData {
+	email: String,
+	name: String
+}
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
 	HttpResponse::Ok().finish()
 }
 
