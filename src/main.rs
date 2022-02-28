89fc15cdcf4670ae2a4e0ac787517d734d6b7d27
kelvinkirima014 <kelvinkirima2@gmail.com>
2022-02-28 11:38:44 +0300

    remove initial greet fn

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,5 @@
 use actix_web::{web, App, HttpRequest, HttpServer, HttpResponse, Responder};
 
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
-}
 
 async fn health_check() -> impl Responder {
     HttpResponse::Ok()
