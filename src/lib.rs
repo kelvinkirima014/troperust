d3382c71ecb7481a845470cd67b67c2eceb991a3
kelvinkirima014 <kelvinkirima2@gmail.com>
2022-03-07 07:44:46 +0300

    add tests file

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@ async fn health_check() -> impl Responder {
 pub async fn run() -> std::io::Result<()> {
 	HttpServer::new(|| {
 	    App::new()
-	       .route("health_check", web::get().to(health_check))
+	       .route("/health_check", web::get().to(health_check))
 	})
 	.bind(("127.0.0.1", 8000))?
 	.run()
