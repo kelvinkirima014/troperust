9b874e6a213ad3b1c0ba85edc36a05002b9cdcd3
kelvinkirima014 <kelvinkirima2@gmail.com>
2022-03-08 08:24:33 +0300

    add subscribe fn to parse form data from a POST req

--- src/lib.rs
+++ src/lib.rs
@@ -7,10 +7,15 @@ async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
+async fn subscribe() -> HttpResponse {
+	HttpResponse::Ok().finish()
+}
+
 pub fn run(listener: TcpListener) -> Result<Server, io::Error> {
 	let server = HttpServer::new(|| {
 		App::new()
 		.route("/health_check", web::get().to(health_check))
+		.route("/subsriptions", web::post().to(subscribe))
 		})
 		.listen(listener)?
 		.run();
