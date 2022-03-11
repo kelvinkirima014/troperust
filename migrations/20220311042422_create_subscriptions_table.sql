e600385801c78b0076059d3f282312962e1e314b
kelvinkirima014 <kelvinkirima2@gmail.com>
2022-03-11 07:31:08 +0300

    add initial migrations sql query

--- migrations/20220311042422_create_subscriptions_table.sql
+++ migrations/20220311042422_create_subscriptions_table.sql
@@ -1,1 +1,9 @@
 -- Add migration script here
+--create Subscriptions Table
+CREATE TABLE Subscriptions(
+	id uuid NOT NULL,
+	PRIMARY KEY (id),
+	email TEXT NOT NULL UNIQUE,
+	name TEXT NOT NULL,
+	subsribed_at timestampz NOT NULL
+);
