8c3f3841a913ecd596cd8084c26438183330fe69
kelvinkirima014 <kelvinkirima2@gmail.com>
2022-03-15 08:24:06 +0300

    modify main function to read configuration as its first step

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 use troperust::startup::run;
 use std::net::TcpListener;
+use troperust::configuration::get_configuration;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let address = TcpListener::bind("127.0.0.1:8000").unwrap();
+    //panic if we can't read configuration
+    let configuration = get_configuration().expect("Failed to read configuration");
+    //remove hardcoded `8000` -it's now coming from settings
+    let address = TcpListener::bind("127.0.0.1:{}").unwrap();
     run(address)?.await
 }
