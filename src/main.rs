dfcad1f9ee19868d5b62472c0f62de3c5d0fe021
kelvinkirima014 <kelvinkirima2@gmail.com>
2022-03-22 08:32:51 +0300

    edit main to read config properly

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ async fn main() -> std::io::Result<()> {
     //panic if we can't read configuration
     let configuration = get_configuration().expect("Failed to read configuration");
     //remove hardcoded `8000` -it's now coming from settings
-    let address = TcpListener::bind("127.0.0.1:{}").unwrap();
-    run(address)?.await
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
+    run(listener)?.await
 }
