036f1231be508ecada9f43e92ef5d74feb107874
kelvinkirima014 <kelvinkirima2@gmail.com>
2022-03-23 08:59:31 +0300

    add app_data to register a postgress connection

--- src/startup.rs
+++ src/startup.rs
@@ -1,15 +1,19 @@
 use crate::routes::{health_check, subscribe};
 use actix_web::{HttpServer, web, App};
 use actix_web::dev::Server;
+use sqlx::PgConnection;
 use std::net::TcpListener;
 use std::io;
 
-pub fn run(listener: TcpListener) -> Result<Server, io::Error> {
+pub fn run(listener: TcpListener,
+	   connection: PgConnection
+	  ) -> Result<Server, io::Error> {
 	let server = HttpServer::new(|| {
 		App::new()
 		.route("/health_check", web::get().to(health_check))
 		.route("/subscriptions", web::post().to(subscribe))
-		})
+		//Register the connection as part of app state
+		.app_data(connection)
 		.listen(listener)?
 		.run();
 	
