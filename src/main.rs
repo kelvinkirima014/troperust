6b78a2a357046849634682962186016985f418a2
kelvinkirima014 <kelvinkirima2@gmail.com>
2022-03-23 09:29:51 +0300

    use sqlx on mai

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,11 @@ use sqlx::{ Connection, PgConnection};
 async fn main() -> std::io::Result<()> {
     //panic if we can't read configuration
     let configuration = get_configuration().expect("Failed to read configuration");
-    //remove hardcoded `8000` -it's now coming from settings
+    let connection = PgConnection::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres");
+    
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    run(listener)?.await
+    run(listener, connection)?.await
 }
