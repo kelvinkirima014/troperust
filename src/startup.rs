dd0178d98c2e0884a82088718c1ce84f33c47bf9
kelvinkirima014 <kelvinkirima2@gmail.com>
2022-03-24 11:50:42 +0300

    use pgPool

--- src/startup.rs
+++ src/startup.rs
@@ -1,21 +1,21 @@
 use crate::routes::{health_check, subscribe};
 use actix_web::{HttpServer, web, App};
 use actix_web::dev::Server;
-use sqlx::PgConnection;
+use sqlx::PgPool;
 use std::net::TcpListener;
 use std::io;
 
 pub fn run(listener: TcpListener,
-	   connection: PgConnection
+	   db_pool: PgPool
 	) -> Result<Server, io::Error> {
-		let connection = web::Data::new(connection);
+		let db_pool = web::Data::new(db_pool);
 		//capture connection from the surrounding environment
 		let server = HttpServer::new(move || {
 			App::new()
 			.route("/health_check", web::get().to(health_check))
 			.route("/subscriptions", web::post().to(subscribe))
 			//Register the connection as part of app state
-			.app_data(connection.clone())
+			.app_data(db_pool.clone())
 		})
 			.listen(listener)?
 			.run();
