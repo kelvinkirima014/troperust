2f5aee569836432301ad5767d09e177ecda28bd6
kelvinkirima014 <kelvinkirima2@gmail.com>
2022-03-24 11:07:51 +0300

    bind dynamic data to insert query

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -12,5 +12,20 @@ pub async fn subscribe(
 	//retrieving a connection from app state
 	_connection: web::Data<PgConnection>
 	) -> HttpResponse {
-	HttpResponse::Ok().finish()
+		sqlx::query(
+			r#"
+			INSERT INTO subscriptions(
+
+			id, email, name,subscribed_at	
+			)
+			VALUES ($1, $2, $3, $4)
+			"#,
+			Uuid::new_v4(),
+			form.email,
+			form.name,
+			Utc::now()
+		)
+		//`get_ref` gets an immutable reference to PgConnection
+		.execute(connection.get_ref()).await;
+		HttpResponse::Ok().finish()
 }
