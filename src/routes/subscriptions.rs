7a692feaf383bc52a8edee6cfdc0d7e60461ff75
kelvinkirima014 <kelvinkirima2@gmail.com>
2022-05-03 20:39:11 +0300

    healcheck/subscribtion rework

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -9,26 +9,6 @@ pub struct FormData {
 	name: String
 }
 
-pub async fn subscribe(
-	form: web::Form<FormData>,
-	//retrieving a connection from app state
-	pool: web::Data<PgPool>,
-	) -> HttpResponse {
-		sqlx::query!(
-			r#"
-			INSERT INTO subscriptions(
-
-			id, email, name,subscribed_at	
-			)
-			VALUES ($1, $2, $3, $4)
-			"#,
-			Uuid::new_v4(),
-			form.email,
-			form.name,
-			Utc::now()
-		)
-		//`get_ref` gets an immutable reference to PgConnection
-		.execute(pool.get_ref())
-		.await;
-		HttpResponse::Ok().finish()
+pub async fn subscribe() {
+	todo!()
 }
